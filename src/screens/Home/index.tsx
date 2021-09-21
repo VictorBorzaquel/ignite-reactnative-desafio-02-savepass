a7d622bb516c64632045dac7feebb62c12c72f5a
VictorBorzaquel <victorborzaquel@outlook.com>
2021-09-21 19:43:52 -0300

    complete

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,15 +30,32 @@ export function Home() {
 
   async function loadData() {
     const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+
+    const response = await AsyncStorage.getItem(dataKey);
+
+    if (response) {
+      const parsedData = JSON.parse(response);
+
+      setData(parsedData);
+      setSearchListData(parsedData);
+    }
   }
 
   function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
+    if (!searchText) {
+      setSearchListData(data)
+      return
+    };
+
+    const searchData = data.filter(item => item.service_name.toLowerCase().includes(searchText.toLowerCase()));
+
+    setSearchListData(searchData);
   }
 
   function handleChangeInputText(text: string) {
-    // Update searchText value
+    if (!text) setSearchListData(data);
+
+    setSearchText(text);
   }
 
   useFocusEffect(useCallback(() => {
